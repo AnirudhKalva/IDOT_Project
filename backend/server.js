a15e83e42435ca127e9bae23b743ca898b8cabb8
Anirudh <anirudhkalva09@gmail.com>
2025-08-10 21:15:35 -0500

    fixed server deployment issue
    fixed server deployment issue

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
-const http = require('http');
-const app = require('./app');
+const http = require("http");
+const app = require("./app");
 
-const port = process.env.PORT || 8083;
+const PORT = process.env.PORT || 8083;
+const HOST = "0.0.0.0"; // Ensure it's reachable from outside the container
+
 const server = http.createServer(app);
 
-server.listen(port, () => {
-  console.log('Server is running on port ' + port);
+server.listen(PORT, HOST, () => {
+  console.log(`Server is running on http://${HOST}:${PORT}`);
 });
+
